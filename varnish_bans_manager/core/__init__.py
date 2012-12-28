75d91a469fcd8cc33afd3b4af98b9f9b7933a851
dot2code Technologies <info@dot2code.com>
2012-12-28 16:15:38 +0100

    Split initialization process for further control.
    Core initialization has been splitted into request intialization, management command
    initialization, task initialization and worker initialization so different things can
    be done in each place.

--- varnish_bans_manager/core/__init__.py
+++ varnish_bans_manager/core/__init__.py
@@ -8,8 +8,25 @@
 from __future__ import absolute_import
 
 
-def initialize():
+def initialize_command():
     _initialize_umask()
+
+
+def initialize_request():
+    _initialize_umask()
+
+
+def initialize_task():
+    _initialize_umask()
+
+
+def initialize_worker():
+    # Media generator URLs need to be refreshed on development mode.
+    # This is mainly needed for sending e-mails with images.
+    from mediagenerator.settings import MEDIA_DEV_MODE
+    from mediagenerator.utils import _refresh_dev_names
+    if MEDIA_DEV_MODE:
+        _refresh_dev_names()
 
 
 def _initialize_umask():
